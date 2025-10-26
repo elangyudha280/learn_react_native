8eb4f6b2badec34298d3703c2932a1e4bdeed59c
elangyudha280 <elangyudharakasiwi2@gmail.com>
2025-10-26 20:13:21 +0700

    set terminologi

---
+++ 7.navigation/2.expo-router/terminologi.js
@@ -0,0 +1,41 @@
+
+
+
+//! MATERI EXPO ROUTER 
+
+
+//! 1. APA ITU EXPO ROUTER
+/*
+    * EXPO ROUTER ADALAH PKG ATAU LIBRARY BAWAAN EXPO UNTUK MEMBANTU KITA DALAM MEMBUAT DAN MENGATUR ROUTER berbasis file
+*/
+//TODO:NOTE: JADI ROUTER DI EXPO ITU. FILE BASE ATAU kita buat menjadi file di dalam folder app
+
+
+//! 2. cara menggunakan expo router
+/*
+    * lebih lengkap bisa cek di dokumentasinya
+
+    * 1. instal pkgnya dulu
+    * 2. buat file di dalam folder app nama.{js,ts,jsx,tsx} -> nanti nama file akan otomatis menjadi nama route atau path
+    * 
+    * 
+    
+    todo: _layout.{js,ts,jsx,tsx} = file yg digunakan untuk membuat layout 
+    todo: +not-found.{js,ts,jsx,tsx} = file untk menentukan halaman notfound
+    todo: +html.{js,ts,jsx,tsx} = file untk membuat sturktur html hanya untuk web
+    todo: +middleware.ts.{js,ts,jsx,tsx} = file untk menentukan aksi sebelum response ke user
+    todo: index.{js,ts,jsx,tsx} -> file untuk menandakan bahwa file tersebut akan menjadi root path
+
+    todo: component yg biasanya di pake
+    * 1. <Link></Link> = compnent untuk mengarahkan user ke halaman lain
+    * 2. <Stack><Stack/> = component untuk mensetup routing dengan gaya stack
+    
+
+    todo:object yg biasanya di pake
+    * 1. router.method() => fungsi yg digunakan untuk mengarahakan user ke halaman lain
+    * 2. useLocalSearchParams() = hooks yg digunakan untuk mengambil data paramter
+
+    
+    todo: kita bisa membuat group routing dengan cara kasih  (nama folder)/..router_file
+*/
+
