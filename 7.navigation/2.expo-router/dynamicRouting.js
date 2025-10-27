717963e6ab366a82aedd7ced6370ee0bae1a0330
elangyudha280 <elangyudharakasiwi2@gmail.com>
2025-10-27 10:10:14 +0700

    set terminologi

---
+++ 7.navigation/2.expo-router/dynamicRouting.js
@@ -0,0 +1,22 @@
+
+
+//! materi dynamic routing di expo router
+
+
+//! 1.1 apa itu dynamic routing
+/*
+    * dynamic routing adalah sebuah cara untuk membuat route yg berubah ubah 
+*/
+//todo: jadi kita bisa mengirim data parameter ke route tersebut
+
+
+
+// ! 2. cara membuat dynamic route
+/*
+
+    * [nama_params].{js,ts,jsx,tsx}
+
+    * catch all segment atau paramter [...nama_params].{js,ts,jsx,tsx}
+
+
+*/
---
+++ 7.navigation/2.expo-router/notfound.js
@@ -0,0 +1,17 @@
+
+
+
+//! materi notfound di expo router
+
+
+//! 1. not found
+/*
+    * notfound adalah sebuah file yg digunakan yg ditampilkan saat routernya ga ada
+*/
+
+//! 2. cara menggunakannya
+/*
+
+* buat file +not-found.{ts,js,tsx,jsx} di dalam folder app
+
+*/
